0b0c1b6f46d4a3ed09101229967ecce04ddf4e04
kjaron83 <kjaron83@gmail.com>
2019-10-04 16:32:16 +0200

    Extending API functionality.

--- src/main/java/com/abixe/hangouttips/ApiController.java
+++ src/main/java/com/abixe/hangouttips/ApiController.java
@@ -3,15 +3,20 @@ package com.abixe.hangouttips;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.lang.NonNull;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.abixe.hangouttips.model.IpLocation;
 import com.abixe.hangouttips.model.Location;
+import com.abixe.hangouttips.service.IpLocationService;
 import com.abixe.hangouttips.service.LocationService;
 import com.abixe.hangouttips.service.PlaceApiService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -21,9 +26,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RequestMapping("api")
 public class ApiController {
 
+    private IpLocationService ipLocationService;
     private LocationService locationService;
     private PlaceApiService placeApiService;
 
+    @Autowired()
+    @Qualifier(value = "ipLocationService")
+    public void setIpLocationService(IpLocationService ipLocationService) {
+        this.ipLocationService = ipLocationService;
+    }    
+    
     @Autowired()
     @Qualifier(value = "locationService")
     public void setLocationService(LocationService locationService) {
@@ -37,15 +49,44 @@ public class ApiController {
     }
 
     @GetMapping(value = "/{country}/{city}", produces = "application/json")
-    public String pullPerson(@PathVariable String country, @PathVariable String city) {
+    public String pullLocationIsExpired(@PathVariable String country, @PathVariable String city) {
         Location location = locationService.get(country + "/" + city);
 
         return json(
                 createResults(location != null && !placeApiService.isExpired(location))
                 );
     }
+    
+    @GetMapping(value = "/test")
+    public String pullIpLocationInfo(Model model, HttpServletRequest request) {
+        String ip = request.getRemoteAddr();
+        IpLocation ipLocation = ipLocationService.getLocation(ip);
 
-    private Map createResults(boolean success) {
+        Map<String, Object> info = createResults(ipLocation != null);
+        info.put("ip", ip);
+        if ( ipLocation != null ) {
+            Location location = locationService.get(ipLocation);
+            info.put("location", location.getPath());
+            info.put("places", location.getPlaces().size());
+            info.put("expired", placeApiService.isExpired(location));
+        }
+        model.addAttribute("info", info);
+
+        return json(info);
+    }    
+
+    @GetMapping(value = "/{country}/{city}/update", produces = "application/json")
+    public String pullLocationUpdate(@PathVariable String country, @PathVariable String city) {
+        Location location = locationService.get(country + "/" + city);
+        if ( location != null )
+            placeApiService.update(location);
+
+        return json(
+                createResults(location != null)
+                );
+    }
+
+    private Map<String, Object> createResults(boolean success) {
         LinkedHashMap<String, Object> map = new LinkedHashMap<>();
         map.put("success", success);
         return map;
