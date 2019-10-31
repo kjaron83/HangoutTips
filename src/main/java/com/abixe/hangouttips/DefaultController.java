1deca2aa3410992e2db9280d31f11b7afee10907
kjaron83 <kjaron83@gmail.com>
2019-10-31 09:21:00 +0100

    Introducing getAbsoluteUrl method in DefaultController class.

--- src/main/java/com/abixe/hangouttips/DefaultController.java
+++ src/main/java/com/abixe/hangouttips/DefaultController.java
@@ -17,6 +17,11 @@ import org.springframework.lang.Nullable;
  */
 public abstract class DefaultController {
 
+    private static final String PROTOCOL_HTTP = "http";
+    private static final String PROTOCOL_HTTPS = "https";
+
+    private static String REDIRECT_PREFIX = "redirect:";
+
     @Value("${root}")
     private String root;
 
@@ -24,18 +29,40 @@ public abstract class DefaultController {
     private HttpServletRequest request;
 
     /**
-     * Returns an absolute URL from the specified path, with a redirect prefix.
+     * Returns an absolute URL from the specified path.
      */
-    public String redirect(@Nullable String to) {
+    public String getAbsoluteUrl(@Nullable String to) {
+        return getAbsoluteUrl(to, null);
+    }
+
+    /**
+     * Returns an absolute URL from the specified path, with a prefix.
+     */
+    public String getAbsoluteUrl(@Nullable String to, @Nullable String prefix) {
         if ( to == null )
             to = "/";
 
-        return new StringBuilder("redirect:")
-                .append(request.isSecure() ? "https" : "http")
+        int size = PROTOCOL_HTTPS.length() + root.length() + to.length();
+        if ( prefix != null )
+            size += prefix.length();
+
+        StringBuilder sb = new StringBuilder(size);
+        if ( prefix != null )
+            sb.append(prefix);
+
+        return sb
+                .append(request.isSecure() ? PROTOCOL_HTTPS : PROTOCOL_HTTP)
                 .append("://")
                 .append(root)
                 .append(to)
                 .toString();
+    }
+
+    /**
+     * Returns an absolute URL from the specified path, with a {@link #REDIRECT_PREFIX} prefix.
+     */
+    public String redirect(@Nullable String to) {
+        return getAbsoluteUrl(to, REDIRECT_PREFIX);
     }
 
 }
